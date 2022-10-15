e8ad617ddaf6f5d6133632e8eeec7f5418b09996
Varvara <varyagagarina@gmail.com>
2022-10-15 13:59:53 +0300

    add: solutions com-car/sim-sor/tr-arr

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let array1 = s1.split('');
+  let array2 = s2.split('');
+  let commonCar = 0;
+  let index;
+
+  array1.filter((item1) => {
+    index = array2.findIndex(item2 => item2 === item1);
+    // Метод findIndex вызывает переданную функцию callback один раз для каждого элемента, присутствующего в массиве, до тех пор, пока она не вернёт true. Если такой элемент найден, метод findIndex немедленно вернёт индекс этого элемента. В противном случае, метод findIndex вернёт -1. 
+    if (index >= 0) {
+      commonCar++;
+      array2.splice(index, 1);
+    }
+  });
+  return commonCar; // сравнение двух массивов
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr.length === 0) return arr;
+  let cloneArr = [...arr];
+
+  for (let i = 0; i < cloneArr.length; i++) {
+    
+    switch (cloneArr[i]) {
+      case "--discard-next":
+        cloneArr.splice(i, 2);
+        if (cloneArr[i] === "--double-prev" || cloneArr[i] === "--discard-prev") {
+          cloneArr.splice(i, 1);
+        }
+      break;        
+      case "--discard-prev": 
+        if (i > 0) {
+          cloneArr.splice(i-1, 2);
+        } else {
+          cloneArr.splice(i, 1);
+        }
+      break;
+      case "--double-next": 
+       if (i < cloneArr.length -1) {
+        cloneArr[i] = cloneArr[i+1];
+       } else {
+        cloneArr.splice(i, 1);
+       }
+      break;
+      case "--double-prev": 
+      if (i > 0) {
+        cloneArr[i] = cloneArr[i-1];
+      } else {
+        cloneArr.splice(i, 1);
+      }
+      break;
+      default: 
+      cloneArr = cloneArr;
+      break;
+    }
+  }
+return cloneArr;
 }
 
 module.exports = {
